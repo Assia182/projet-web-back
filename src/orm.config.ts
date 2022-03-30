80f232350456c0d0662b0b2f29765d5d613d9d1f
eyaJLASSI <jelassieya9@gmail.com>
2022-03-30 17:35:52 +0200

    db

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const config: TypeOrmModuleOptions = {
     type: 'postgres',
-    username: 'postgres',
-    password: 'postgres',
-    port: 5432,
-    host: 'localhost',
-    database: 'beaute_de_lorient', 
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
+    host: process.env.PROSTGRES_HOST,
+    database: process.env.POSTGRES_DB, 
     schema: 'beaute_de_lorient',
     synchronize: true,
     entities: ["dist/**/entities/*.entity{.ts,.js}"]
